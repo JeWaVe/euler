d0e81f39c61e83180a00c509d7a3bec3c62b2f2b
Portalez Régis <regis.portalez@altimesh.com>
2019-05-27 06:56:47 +0100

    Update Source.cpp
    add example of taylor<...>

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,19 @@
 #include "euler.h"
 #include <cstdio>
 
+template<int64_t index>
+struct my_coeff {
+	using type = euler::rational<1, 1>;
+};
+
+// 1/(1-x)
+template<typename T, int64_t d>
+using serie = euler::taylor<T, my_coeff, d>;
+
 int main()
 {
-	constexpr euler::exp<double, 15> e;
-	constexpr auto result = e(3.0);
+	constexpr auto a = euler::integers::binomial<12, 128>::val;
+	constexpr serie<double, 15> e;
+	constexpr auto result = e(0.1);
 	::printf("%.17lf\n", result);
 }
